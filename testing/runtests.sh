aa634ada10b5e57d4cb3b272525e8c707a8f0c64
Sam Boyer (sdboyer) <sam@samboyer.org>
2009-05-29 10:32:16 -0500

    Better descriptive strings.

--- testing/runtests.sh
+++ testing/runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d 'repo' ]; then
-  echo "Creating testing repository"
+  echo "Creating test repository"
   mkdir repo
   svnadmin create repo > /dev/null 2>&1
   test -f repo.svnadmin.bz2 && bunzip2 repo.svnadmin.bz2
@@ -9,6 +9,7 @@ if [ ! -d 'repo' ]; then
 fi
 
 if [ ! -d 'wc' ]; then
+  echo "Checking out test working copy"
   svn co file:///`pwd`/repo wc
 fi
 
