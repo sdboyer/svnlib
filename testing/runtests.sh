90d121c47de5ddb21a1cc7dae7d440ff74a48b33
Sam Boyer (sdboyer) <sam@samboyer.org>
2009-05-29 17:53:13 -0500

    Made the testing script work properly irrespective of where it is called from; it now figures out the proper path and works from there.

--- testing/runtests.sh
+++ testing/runtests.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+# ensure we're invoking from the right place, or else it burps
+LSOF=$(lsof -p $$ | grep -E "/"$(basename $0)"$")
+MY_PATH=$(echo $LSOF | sed -r s/'^([^\/]+)\/'/'\/'/1 2>/dev/null)
+if [ $? -ne 0 ]; then
+## OSX
+  MY_PATH=$(echo $LSOF | sed -E s/'^([^\/]+)\/'/'\/'/1 2>/dev/null)
+fi
+root=$(dirname $MY_PATH)
+cd $root
 
-if [ ! -d 'repo' ]; then
+
+if [ ! -d "$root/repo" ]; then
   echo "Creating test repository"
-  mkdir repo
-  svnadmin create repo > /dev/null 2>&1
-  test -f repo.svnadmin.bz2 && bunzip2 repo.svnadmin.bz2
-  svnadmin load repo < repo.svnadmin > /dev/null 2>&1
+  mkdir $root/repo
+  svnadmin create $root/repo > /dev/null 2>&1
+  test -f $root/repo.svnadmin.bz2 && bunzip2 $root/repo.svnadmin.bz2
+  svnadmin load $root/repo < $root/repo.svnadmin > /dev/null 2>&1
 fi
 
-if [ ! -d 'wc' ]; then
+if [ ! -d "$root/wc" ]; then
   echo "Checking out test working copy"
-  svn co file:///`pwd`/repo wc
+  svn co file://$root/repo wc
 fi
 
-phpunit tests
+phpunit $root/tests
