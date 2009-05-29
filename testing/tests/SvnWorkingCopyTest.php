d5edd49e8d6bb529570c339b4dba0215d98c0e82
Sam Boyer (sdboyer) <sam@samboyer.org>
2009-05-29 10:32:49 -0500

    Incremental commit of testing goodies.

--- testing/tests/SvnWorkingCopyTest.php
+++ testing/tests/SvnWorkingCopyTest.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'PHPUnit/Framework.php';
 require_once '../src/svn.php';
-class SvnWorkingCopyTest extends PHPUnit_Framework_TestCase {
+class SvnWorkingCopyInitTest extends PHPUnit_Framework_TestCase {
   /**
    * @expectedException InvalidArgumentException
    */
@@ -10,9 +10,25 @@ class SvnWorkingCopyTest extends PHPUnit_Framework_TestCase {
   }
 
    /**
-   * @expectedException InvalidArgumentException 
+   * @expectedException InvalidArgumentException
    */
   public function testVerifyFailureNotSvnDir() {
     new SvnWorkingCopy('.');
+  }
+
+  public function testAutoSubPathing() {
+    $wc = new SvnWorkingCopy('wc/trunk');
+
+  }
+}
+
+class SvnWorkingCopyTest extends PHPUnit_Framework_TestCase {
+  public function setUp() {
+    $this->wc = new SvnWorkingCopy('./wc');
+  }
+
+  public function testInfoBuild() {
+    $this->assertEquals(getcwd() . DIRECTORY_SEPARATOR . 'repo', $this->wc->getRepoRoot(), "Incorrect repository root was retrieved.");
+    $this->assertEquals(340, $this->wc->getLatestRev(), "Incorrect latest revision was retrieved.");
   }
 }
