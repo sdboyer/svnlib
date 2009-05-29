68ab445b4134af5f1ba519a4634ed2da7ae5135e
Sam Boyer (sdboyer) <sam@samboyer.org>
2009-05-29 10:06:39 -0500

    Added initialization of test data to the test running script

--- testing/runtests.sh
+++ testing/runtests.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+if [ ! -d 'repo' ]; then
+  echo "Creating testing repository"
+  mkdir repo
+  svnadmin create repo > /dev/null 2>&1
+  test -f repo.svnadmin.bz2 && bunzip2 repo.svnadmin.bz2
+  svnadmin load repo < repo.svnadmin > /dev/null 2>&1
+fi
+
+if [ ! -d 'wc' ]; then
+  svn co file:///`pwd`/repo wc
+fi
+
 phpunit tests
